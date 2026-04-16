51f1a3bdf766420dccddd393b488f9f80c063c3e
LaGregance <gregoire.taja@gmail.com>
2026-04-16 11:37:14 +0200

    WIP: Monorepo

--- packages/test-app/src/App.tsx
+++ packages/test-app/src/App.tsx
@@ -1,9 +1,7 @@
-import "./config/unistyles";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./navigation/root.navigator";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatelessDialogProvider } from "@react-stateless-dialog/core";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ViewStyle } from "react-native";
 import { statelessDialogConfig } from "./config/react-stateless-dialog";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +12,6 @@ import { queryClient } from "./config/query-client";
 import { MobilyPurchaseSDK } from "mobilyflow-react-native-sdk";
 
 const SAFE_AREA: ViewStyle = { flex: 1, backgroundColor: "black" };
-const FLEX: ViewStyle = { flex: 1 };
 
 export default function App() {
   useEffect(() => {
@@ -47,13 +44,11 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
         <SafeAreaView style={SAFE_AREA}>
-          <GestureHandlerRootView style={FLEX}>
-            <NavigationContainer>
-              <StatelessDialogProvider config={statelessDialogConfig}>
-                <RootNavigator />
-              </StatelessDialogProvider>
-            </NavigationContainer>
-          </GestureHandlerRootView>
+          <NavigationContainer>
+            <StatelessDialogProvider config={statelessDialogConfig}>
+              <RootNavigator />
+            </StatelessDialogProvider>
+          </NavigationContainer>
         </SafeAreaView>
       </SafeAreaProvider>
     </QueryClientProvider>
